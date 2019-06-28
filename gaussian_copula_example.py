adfa277eb65f9484bbb83e24f23b50a8f12b75bd
Kevin <kdoherty@mit.edu>
2019-06-28 19:38:13 -0400

    Fixed up Gaussian copula example

--- gaussian_copula_example.py
+++ gaussian_copula_example.py
@@ -1,3 +1,4 @@
+import math
 import tensorflow as tf
 import tensorflow_probability as tfp
 import copula_gaussian as cg
@@ -6,29 +7,29 @@ import seaborn as sb
 
 # x CDF is a Gaussian mixture with two components
 x_probs = [0.5, 0.5]
-x_locs = [1.0, 2.0]
-x_scales = [0.1, 0.1]
+x_locs = [1.0, 4.0]
+x_scales = [0.5, 0.5]
 
 # y CDF is a Gaussian
 y_loc = 1.0
-y_scale = 0.1
+y_scale = 1.0
 
 # Correlation parameter
-rho = 0.5
+rho = 0.9
 
 # Correlation matrix
-L_mat = []
+L_mat = tf.constant([[1., 0.], [rho, math.sqrt(1. - rho**2)]])
 
 # Number of samples
-n_samples = 100
+n_samples = 100000
 
 # Make CDF bijectors for each marginal
-x_cdf = cg.EmpGaussianMixtureCDF(probs=x_probs, locs=x_locs, scales=x_scales)
+x_cdf = cg.EmpGaussianMixtureCDF(ps=x_probs, locs=x_locs, scales=x_scales)
 y_cdf = cg.NormalCDF(loc=y_loc, scale=y_scale)
 
 # Build copula
 gc = cg.WarpedGaussianCopula(
-    loc=[0., 0., 0.],
+    loc=[0., 0.],
     scale_tril=L_mat,
     marginal_bijectors=[x_cdf, y_cdf])
 
@@ -36,5 +37,5 @@ gc = cg.WarpedGaussianCopula(
 sess = tf.Session()
 
 samples = gc.sample(n_samples).eval(session=sess)
-sb.kdeplot(samples)
+sb.jointplot(samples[:,0], samples[:,1], kind='kde')
 plt.show()
